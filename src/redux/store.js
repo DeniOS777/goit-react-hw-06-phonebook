e0fbdb16b265d0905c67c852413881df84ccef6f
DeniOS777 <Denios777@gmail.com>
2022-09-24 19:35:03 +0300

    clear comments in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,7 +10,6 @@ import {
   REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import contactsSlice from './contacts/contactsReducer';
 import { itemsSlice } from './contacts/itemsSlice';
 import { filterSlice } from './contacts/filterSlice';
 
